cbee15cef5770cb64754eb735d3bc7050e44354d
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2024-06-25 00:03:49 +0200

    Autolandscape Material

---
+++ Source/TemplateBase/Private/World/Weather/DynamicSky.cpp
@@ -0,0 +1,150 @@
+// Retropsis @ 2024
+
+#include "World/Weather/DynamicSky.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/ExponentialHeightFogComponent.h"
+#include "Components/PostProcessComponent.h"
+#include "Components/SkyAtmosphereComponent.h"
+#include "Components/SkyLightComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+ADynamicSky::ADynamicSky()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+
+	SunDirectionalLight = CreateDefaultSubobject<UDirectionalLightComponent>("SunDirectionalLight");
+	SunDirectionalLight->ForwardShadingPriority = 1;
+	SunDirectionalLight->SetupAttachment(GetRootComponent());
+	SunDirectionalLight->SetRelativeRotation(FRotator(-75.f, 0.f, 0.f));
+	
+	MoonDirectionalLight = CreateDefaultSubobject<UDirectionalLightComponent>("MoonDirectionalLight");
+	MoonDirectionalLight->ForwardShadingPriority = 1;
+	MoonDirectionalLight->SetupAttachment(GetRootComponent());
+	
+	SkyAtmosphere = CreateDefaultSubobject<USkyAtmosphereComponent>("SkyAtmosphere");
+	SkyAtmosphere->SetupAttachment(GetRootComponent());
+	
+	SkyLight = CreateDefaultSubobject<USkyLightComponent>("SkyLight");
+	SkyLight->SetRealTimeCaptureEnabled(true);
+	SkyLight->SetupAttachment(GetRootComponent());
+
+	ExponentialHeightFog = CreateDefaultSubobject<UExponentialHeightFogComponent>("ExponentialHeightFog");
+	ExponentialHeightFog->SetupAttachment(GetRootComponent());
+	
+	PostProcessVolume = CreateDefaultSubobject<UPostProcessComponent>("PostProcessVolume");
+	PostProcessVolume->SetupAttachment(GetRootComponent());
+	PostProcessVolume->Settings.AutoExposureMinBrightness = .5f;
+	PostProcessVolume->Settings.AutoExposureMaxBrightness = .5f;
+	PostProcessVolume->Settings.bOverride_AutoExposureMinBrightness = true;
+	PostProcessVolume->Settings.bOverride_AutoExposureMaxBrightness = true;
+
+	SkySphere = CreateDefaultSubobject<UStaticMeshComponent>("SkySphere");
+	SkySphere->SetupAttachment(GetRootComponent());
+	SkySphere->SetWorldScale3D(FVector(100000.f));
+}
+
+void ADynamicSky::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if(InitializeSkySphereMaterial())
+	{
+		HandleSunMoonRotation();
+		HandleSunMoonVisibility();
+		HandleNightSettings();
+	}
+}
+
+void ADynamicSky::BeginPlay()
+{
+	Super::BeginPlay();
+	InitializeSkySphereMaterial();
+	DynamicTimeOfDay = TimeOfDay;
+}
+
+void ADynamicSky::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	DynamicTimeOfDay += DeltaTime * DayNightCycleSpeed;
+	double Remainder;
+	const int64 Days = UKismetMathLibrary::FMod64(DynamicTimeOfDay, 24.f, Remainder);
+	GEngine->AddOnScreenDebugMessage(12345, 1.f, FColor::White, FString::Printf(TEXT("Current Time: %f - Days: %lld"), Remainder, Days));
+	const FText TimeFormat = FText::AsTimespan(UKismetMathLibrary::FromHours(Remainder));
+	GEngine->AddOnScreenDebugMessage(12346, 1.f, FColor::White, TimeFormat.ToString());
+
+	TimeOfDay = Remainder;
+	HandleDynamics();
+}
+
+void ADynamicSky::HandleDynamics()
+{
+	HandleSunMoonRotation();
+	HandleSunMoonVisibility();
+	HandleNightSettings();
+}
+
+void ADynamicSky::HandleSunMoonRotation()
+{
+	const float SunPitch = UKismetMathLibrary::MapRangeUnclamped(TimeOfDay, DawnTime, DuskTime, 0.f, -180.f);
+	SunDirectionalLight->SetWorldRotation(FRotator(SunPitch, SunYaw, 0.f));
+	
+	const float MoonPitchAfterDusk = UKismetMathLibrary::MapRangeUnclamped(TimeOfDay, DuskTime, 24.f, -180.f, -90.f);
+	MoonDirectionalLight->SetWorldRotation(FRotator(MoonPitchAfterDusk, MoonYaw, 0.f));
+	
+	const float MoonPitchBeforeDawn = UKismetMathLibrary::MapRangeUnclamped(TimeOfDay, 0.f, DawnTime, -90.f, 0.f);
+	MoonDirectionalLight->SetWorldRotation(FRotator(MoonPitchBeforeDawn, MoonYaw, 0.f));
+}
+
+void ADynamicSky::HandleSunMoonVisibility()
+{
+	SunDirectionalLight->SetVisibility(IsDayTime());
+	MoonDirectionalLight->SetVisibility(IsNightTime());
+}
+
+void ADynamicSky::HandleNightSettings()
+{
+	if(IsNightTime())
+	{
+		MoonDirectionalLight->SetIntensity(1.f);
+		MoonDirectionalLight->SetLightColor(MoonLightColor);
+		MoonDirectionalLight->SetLightSourceAngle(MoonLightSourceAngle);
+		MoonDirectionalLight->SetUseTemperature(true);
+		MoonDirectionalLight->Temperature = MoonLightTemperature;
+
+		SkyAtmosphere->SetRayleighScattering(NightTimeRayleighScattering);
+		SkyAtmosphere->SetMultiScatteringFactor(NightTimeMultiScatteringFactor);
+		
+		DynamicSkySphereMaterial->SetScalarParameterValue(FName("IsNightSkyVisible"), bIsNightSkyVisible ? 1.0 : 0.f);
+		DynamicSkySphereMaterial->SetScalarParameterValue(FName("IsMoonVisible"), bIsMoonVisible ? 1.0 : 0.f);
+		DynamicSkySphereMaterial->SetVectorParameterValue(FName("MoonBasicSettings"), FLinearColor(MoonScale, MoonOrientation, MoonBrightness, 1.f));
+		DynamicSkySphereMaterial->SetVectorParameterValue(FName("MoonTint"), MoonTint);
+	}
+	else
+	{
+		SkyAtmosphere->SetRayleighScattering(DayTimeRayleighScattering);
+		SkyAtmosphere->SetMultiScatteringFactor(1.f);
+		DynamicSkySphereMaterial->SetScalarParameterValue(FName("IsNightSkyVisible"), 0.f);
+		DynamicSkySphereMaterial->SetScalarParameterValue(FName("IsMoonVisible"), 0.f);
+	}
+}
+
+bool ADynamicSky::InitializeSkySphereMaterial()
+{
+	if(SkySphereMaterial)
+	{
+		DynamicSkySphereMaterial = SkySphere->CreateDynamicMaterialInstance(0, SkySphereMaterial);
+	}
+	return IsValid(DynamicSkySphereMaterial);
+}
+
+bool ADynamicSky::IsDayTime()
+{
+	return TimeOfDay > DawnTime && TimeOfDay < DuskTime;
+}
+
+bool ADynamicSky::IsNightTime()
+{
+	return !IsDayTime();
+}
+
---
+++ Source/TemplateBase/Public/World/Weather/DynamicSky.h
@@ -0,0 +1,132 @@
+// Retropsis @ 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DynamicSky.generated.h"
+
+class UPostProcessComponent;
+
+UCLASS()
+class TEMPLATEBASE_API ADynamicSky : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ADynamicSky();
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void HandleDynamics();
+
+	UFUNCTION(BlueprintCallable)
+	void HandleSunMoonRotation();
+
+	UFUNCTION(BlueprintCallable)
+	void HandleSunMoonVisibility();
+
+	UFUNCTION(BlueprintCallable)
+	void HandleNightSettings();
+
+	UFUNCTION(BlueprintCallable)
+	bool IsDayTime();
+
+	UFUNCTION(BlueprintCallable)
+	bool IsNightTime();
+
+	UFUNCTION(BlueprintCallable)
+	bool InitializeSkySphereMaterial();
+
+	float DynamicTimeOfDay = 0.f;
+
+	UPROPERTY(EditAnywhere, Category="01-Basic Settings")
+	float DayNightCycleSpeed = 10.f;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UDirectionalLightComponent> SunDirectionalLight;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UDirectionalLightComponent> MoonDirectionalLight;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USkyAtmosphereComponent> SkyAtmosphere;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USkyLightComponent> SkyLight;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UExponentialHeightFogComponent> ExponentialHeightFog;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UPostProcessComponent> PostProcessVolume;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> SkySphere;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UMaterialInterface> SkySphereMaterial;
+
+	UPROPERTY()
+	TObjectPtr<UMaterialInstanceDynamic> DynamicSkySphereMaterial;
+	
+	UPROPERTY(EditAnywhere, Category="01-Basic Settings", meta=(ClampMin="0.0", ClampMax="24.0", UIMin ="0.0", UIMax="24.0"))
+	float TimeOfDay = 9.f;
+	
+	UPROPERTY(EditAnywhere, Category="01-Basic Settings", meta=(ClampMin="4.0", ClampMax="11.0", UIMin ="4.0", UIMax="11.0"))
+	float DawnTime = 6.f;
+	
+	UPROPERTY(EditAnywhere, Category="01-Basic Settings", meta=(ClampMin="16.0", ClampMax="23.0", UIMin ="16.0", UIMax="23.0"))
+	float DuskTime = 18.f;
+
+	UPROPERTY(EditAnywhere, Category="01-Basic Settings", meta=(ClampMin="0.0", ClampMax="360.0", UIMin ="0.0", UIMax="360.0"))
+	float SunYaw = 0.f;
+	
+	UPROPERTY(EditAnywhere, Category="01-Basic Settings", meta=(ClampMin="0.0", ClampMax="360.0", UIMin ="0.0", UIMax="360.0"))
+	float MoonYaw = 0.f;
+	
+	UPROPERTY(EditAnywhere, Category="01-Basic Settings")
+	FLinearColor DayTimeRayleighScattering;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	bool bIsNightSkyVisible = true;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	bool bIsMoonVisible = true;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	FLinearColor MoonTint = FLinearColor::White;
+
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	float MoonScale = .15f;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	float MoonOrientation = 1.f;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	float MoonBrightness = 2.f;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	float MoonLightIntensity = 2.f;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	float MoonLightSourceAngle = 0.f;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	float MoonLightTemperature = 8900.f;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	FLinearColor MoonLightColor;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	FLinearColor NightTimeRayleighScattering;
+	
+	UPROPERTY(EditAnywhere, Category="02-Night Settings")
+	float NightTimeMultiScatteringFactor = 0.5f;
+public:	
+
+};
